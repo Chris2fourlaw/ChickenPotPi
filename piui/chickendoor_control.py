f17886c6f19b4d855bb2117663881c6fc61c47e4
Chris <Chris2fourlaw@users.noreply.github.com>
2015-11-19 21:07:43 -0800

    Update chickendoor_control.py

--- piui/chickendoor_control.py
+++ piui/chickendoor_control.py
@@ -7,24 +7,45 @@ import RPi.GPIO as GPIO
 import time
 import signal
 import sys
-import httplib, urllib #for Push Notifications
+import httplib, urllib #for PushOver
+
+#Web Server Config
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 class DoorControl(object):
-  
-  def __init__(self):
-    self.page = self.ui.new_ui_page(title="Control Door")
-    self.title = self.page.add_textbox("Open Or Close Chicken Coop Doors", "h1")
-    self.page.add_textbox("Current Status of Door:", "p")
-    up = self.page.add_button("Open", self.onupclick)
-    down = self.page.add_button("Close", self.ondownclick)
 
-#config.txt included in .gitignore first line is the token, the second line is the key
-config = open('config.txt').readlines()
-pushover_token=config[0].rstrip()
-pushover_user=config[1]
+    def __init__(self):
+        self.title = self.page.add_textbox("Control Chicken Coop Doors!", "h1")
+        self.txt = None
+        self.img = None
+        #self.ui = DoorControl(img_dir=os.path.join(current_dir, 'imgs'))
+        #self.src = "chickencoop.png"
+    
+    def page_buttons(self):    
+        self.page = self.ui.new_ui_page(title="Control Door")
+        self.ui = PiUi()
+        self.title = self.page.add_textbox("Open Or Close Chicken Coop Doors", "h1")
+        self.page.add_textbox("Current Status of Door:", "p")
+        Door_Action = self.page.add_button("Open", self.onupclick)
+        Door_Action = self.page.add_button("Close", self.ondownclick)
+        killSystem = self.page.add_button("Shutdown Door & Server", self.ondownclick)
 
+    def main_menu(self)
+        self.page = self.ui.new_ui_page(title="Control Door")
+        self.list = self.page.add_list()
+        self.list.add_item("Control Door", chevron=True, onclick=self.page_buttons)
+        self.list.add_item("Console", chevron=True, onclick=self.page_console)
+        self.ui.done()
+
+    def main():
+        piui = DoorControl()
+        piui.main()
+
+    if __name__ == '__main__':
+        main()
+
+#GPIO Config
 
 #Setting up Board GPIO Pins
 GPIO.setmode(GPIO.BOARD)
@@ -34,62 +55,69 @@ GPIO.setup(33,GPIO.IN)#Locked
 GPIO.setup(31,GPIO.IN)#Open
 
 #Clean kill of script function (Stops Motor, cleans GPIO)
-def Safe_Kill():
-        print 'Performing safe shutoff!'
+if Door_Action=='up': #Shutdown is queued
+        print 'Performing safe shutoff of Door & Server!'
         GPIO.output(37,False)
         GPIO.output(35,False)
         GPIO.cleanup()
-        sys.exit('Motors shutdown, GPIO cleaned')
+        sys.exit('Motors shutdown, GPIO cleaned, server killed')
+
+#PushOver Config
+
+#config.txt included in .gitignore first line is the token, the second line is the key
+config = open('config.txt').readlines()
+pushover_token=config[0].rstrip()
+pushover_user=config[1]
 
 def PushOver(message):
     conn = httplib.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",
       urllib.urlencode({
-	  #I know, I know no keys in source control. Sheesh.
-          #The old keys no longer work
         "token": pushover_token,
         "user": pushover_user,
         "message": message,
       }), { "Content-type": "application/x-www-form-urlencoded" })
     conn.getresponse()
 
-#Argument controller
-if len(sys.argv)>3: #Tests if you've entered too many arguments
-    print "You've entered too many arguments!"
-    print "Exiting program..."
-    sys.exit(0)
+#Argument controller (Not Needed for Online, so commented out)
 
-if len(sys.argv)>2: #Argument for door action time
-    try:
-        float(sys.argv[2])
-    except:
-        print 'Error: ',str(sys.argv[2]),' is not a number!'
-        print "Exiting program..."
-        sys.exit(0)
-    if int(sys.argv[2])>45: #Checks that a time longer than 45s isn't entered
-            print 'Please choose a time less than 45s'
-            print "Exiting program..."
-            sys.exit(0)
+#if len(sys.argv)>3: #Tests if you've entered too many arguments
+ #   print "You've entered too many arguments!"
+  #  print "Exiting program..."
+   # sys.exit(0)
 
-if len(sys.argv)>1: #Argument for door action
-    if sys.argv[1]!='close' and sys.argv[1]!='open':
-            print 'Please choose "open" or "close"'
-            print "Exiting program..."
-            sys.exit(0)
+#if len(sys.argv)>2: #Argument for door action time
+ #   try:
+  #      float(sys.argv[2])
+   # except:
+    #    print 'Error: ',str(sys.argv[2]),' is not a number!'
+     #   print "Exiting program..."
+      #  sys.exit(0)
+    #if int(sys.argv[2])>45: #Checks that a time longer than 45s isn't entered
+     #       print 'Please choose a time less than 45s'
+      #      print "Exiting program..."
+       #     sys.exit(0)
 
-if len(sys.argv)==3:
-    print 'Forcing door to',str(sys.argv[1]),'for',str(sys.argv[2]),'seconds'
-    Door_Action=sys.argv[1]
-    Door_Time=int(sys.argv[2])
-if len(sys.argv)==2:
-    print 'Forcing door to ',str(sys.argv[1])
-    Door_Action=sys.argv[1]
-    Door_Time=45 #This is a safety time
-if len(sys.argv)==1:
-    Door_Action='default' #Will reverse door state
-    Door_Time=45 #This is a safety time
+#if len(sys.argv)>1: #Argument for door action
+ #   if sys.argv[1]!='close' and sys.argv[1]!='open':
+  #          print 'Please choose "open" or "close"'
+   #         print "Exiting program..."
+    #        sys.exit(0)
+
+#if len(sys.argv)==3:
+ #   print 'Forcing door to',str(sys.argv[1]),'for',str(sys.argv[2]),'seconds'
+    #Door_Action=sys.argv[1]
+   # Door_Time=int(sys.argv[2])
+#if len(sys.argv)==2:
+   # print 'Forcing door to ',str(sys.argv[1])
+    #Door_Action=sys.argv[1]
+   # Door_Time=45 #This is a safety time
+#if len(sys.argv)==1:
+ #   Door_Action='default' #Will reverse door state
+  #  Door_Time=45 #This is a safety time
  
 #Start door!
+
 #def DoorControl():
 TimeStart=time.clock()
 runTime=0
@@ -100,7 +128,7 @@ if BottomHall==0:print 'Door is locked'
 if TopHall==0:print 'Door is open'
 if BottomHall==1:print 'No magnet sensed on lock'
 if TopHall==1:print 'No magnet sensed top'
-if Door_Action=='open': #Door is locked
+if Door_Action=='up': #Door is locked
 		print 'The door is locked!'
 		print 'The door is going up!'
 		while TopHall==1 and runTime<Door_Time:
@@ -118,7 +146,7 @@ if Door_Action=='open': #Door is locked
 				message = 'Coop opened successfully!'
 				PushOver(message)
 				Safe_Kill()
-elif Door_Action=='close': #Door is open
+elif Door_Action=='down': #Door is open
 		print 'The door is open!'
 		print 'The door is going down!'
 		while BottomHall==1 and runTime<Door_Time:
