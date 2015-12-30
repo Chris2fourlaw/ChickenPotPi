6397e649d51ee763b6dc913e94e988af916f8fb0
Chris <Chris2fourlaw@users.noreply.github.com>
2015-12-30 15:55:31 -0800

    Update chickendoor_control.py

--- piui/chickendoor_control.py
+++ piui/chickendoor_control.py
@@ -20,46 +20,48 @@ class DoorControl(object):
         self.title = None
         self.txt = None
         self.img = None
-        #self.ui = DoorControl(img_dir=os.path.join(current_dir, 'imgs'))
-        #self.src = "chickencoop.png"
-    
-    def page_buttons(self):    
+        self.ui = PiUi(img_dir=os.path.join(current_dir, 'imgs'))
+        self.src = "chickens.png"
+
+    def page_buttons(self):
         self.page = self.ui.new_ui_page(title="Control", prev_text="Back", onprevclick=self.main_menu)
-        self.page.add_textbox("Open Or Close Chicken Coop Doors", "h1")
-        up = self.page.add_button("Open", self.onopenclick)
-        down = self.page.add_button("Close", self.oncloseclick)
-        stop = self.page.add_button("Shutdown Door & Server", self.onkillclick)
+        self.title = self.page.add_textbox("Open Or Close Chicken Coop Door!", "h1")
+        up = self.page.add_button("Open &uarr;", self.onupclick)
+        down = self.page.add_button("Close &darr;", self.ondownclick)
+        self.img = self.page.add_image("chickens.png")
 
     def main_menu(self):
-        self.page = self.ui.new_ui_page(title="Chicken Door")
+        self.page = self.ui.new_ui_page(title="Chicken Control Center")
         self.list = self.page.add_list()
-        self.list.add_item("Control Door", chevron=True, onclick=self.page_buttons)
+        self.list.add_item("Control", chevron=True, onclick=self.page_buttons)
         self.ui.done()
 
     def main(self):
         self.main_menu()
         self.ui.done()
 
-    def onopenclick(self):
-	DoorAction = up
+    def onupclick(self):
+        DoorAction = up
+        self.title.set_text("Opening")
+        print "Open"
 
-    def oncloseclick(self):
-	DoorAction = down
-	
+    def ondownclick(self):
+        DoorAction = down
+        self.title.set_text("Closing")
+        print "Close"
+    
     def onkillclick(self):
 	killSystem = '1'
+        self.title.set_text("Killing Server")
+        print "Killing"
         
-    def main(self):
-        self.main_menu()
-        self.ui.done()
-
 def main():
-    piui = DoorControl()
-    piui.main()
+  piui = DoorControl()
+  piui.main()
 
 if __name__ == '__main__':
     main()
-
+    
 #GPIO Config
 
 #Setting up Board GPIO Pins
@@ -99,43 +101,6 @@ def PushOver(message):
       }), { "Content-type": "application/x-www-form-urlencoded" })
     conn.getresponse()
 
-#Argument controller (Not Needed for Online, so commented out)
-
-#if len(sys.argv)>3: #Tests if you've entered too many arguments
- #   print "You've entered too many arguments!"
-  #  print "Exiting program..."
-   # sys.exit(0)
-
-#if len(sys.argv)>2: #Argument for door action time
- #   try:
-  #      float(sys.argv[2])
-   # except:
-    #    print 'Error: ',str(sys.argv[2]),' is not a number!'
-     #   print "Exiting program..."
-      #  sys.exit(0)
-    #if int(sys.argv[2])>45: #Checks that a time longer than 45s isn't entered
-     #       print 'Please choose a time less than 45s'
-      #      print "Exiting program..."
-       #     sys.exit(0)
-
-#if len(sys.argv)>1: #Argument for door action
- #   if sys.argv[1]!='close' and sys.argv[1]!='open':
-  #          print 'Please choose "open" or "close"'
-   #         print "Exiting program..."
-    #        sys.exit(0)
-
-#if len(sys.argv)==3:
- #   print 'Forcing door to',str(sys.argv[1]),'for',str(sys.argv[2]),'seconds'
-    #Door_Action=sys.argv[1]
-   # Door_Time=int(sys.argv[2])
-#if len(sys.argv)==2:
-   # print 'Forcing door to ',str(sys.argv[1])
-    #Door_Action=sys.argv[1]
-   # Door_Time=45 #This is a safety time
-#if len(sys.argv)==1:
- #   Door_Action='default' #Will reverse door state
-  #  Door_Time=45 #This is a safety time
- 
 #Start door!
 
 #def DoorControl():
