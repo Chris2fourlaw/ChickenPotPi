83b3e8139f9ef1b18a855931fb55b2df29635861
Chris <Chris2fourlaw@users.noreply.github.com>
2015-12-31 14:00:30 -0800

    Update chickendoor_control.py

--- piui/chickendoor_control.py
+++ piui/chickendoor_control.py
@@ -21,6 +21,85 @@ GPIO.setup(24,GPIO.OUT) #Buzzer
 GPIO.setup(25,GPIO.OUT) #LED
 GPIO.setup(27,GPIO.IN) #MakeyMakey
 
+#Clean kill of script function (Stops Motor, cleans GPIO)
+def killSystem(): #Shutdown is queued
+        print 'Performing safe shutoff of Door & Server!'
+        GPIO.output(22,False)
+        GPIO.output(23,False)
+        GPIO.output(24,False)
+        GPIO.output(25,False)
+        GPIO.cleanup()
+        sys.exit('Motors shutdown, GPIO cleaned, server killed')
+
+#PushOver Config
+
+#config.txt included in .gitignore first line is the token, the second line is the key
+config = open('config.txt').readlines()
+pushover_token=config[0].rstrip()
+pushover_user=config[1]
+
+def PushOver(message):
+    conn = httplib.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+      urllib.urlencode({
+        "token": pushover_token,
+        "user": pushover_user,
+        "message": message,
+      }), { "Content-type": "application/x-www-form-urlencoded" })
+    conn.getresponse()
+
+#Motor Config
+
+BottomHall=GPIO.input(18)
+TopHall=GPIO.input(17)
+if BottomHall==0:print 'Door is locked'
+if TopHall==0:print 'Door is open'
+if BottomHall==1:print 'No magnet sensed on lock'
+if TopHall==1:print 'No magnet sensed top'
+def openDoor():
+	TimeStart=time.clock()
+	runTime=0
+	if BottomHall==0: #Door is locked
+		print 'The door is locked!'
+		print 'The door is going up!'
+		while TopHall==1 and runTime<Door_Time:
+				GPIO.output(35,True)
+				GPIO.output(37,False)
+				TopHall=GPIO.input(33)
+				runTime=time.clock()-TimeStart
+		if 45==runTime:
+				up = '0'
+				print 'Something went wrong, go check the door!'
+				message = 'Coop open FAILED!'
+				PushOver(message)
+		if TopHall==0:
+				up = '0'
+				print 'Door is open!'
+				message = 'Coop opened successfully!'
+				PushOver(message)
+def closeDoor():
+	TimeStart=time.clock()
+	runTime=0
+	if TopHall==0: #Door is open
+		print 'The door is open!'
+		print 'The door is going down!'
+		while BottomHall==1 and runTime<Door_Time:
+				GPIO.output(35,False)
+				GPIO.output(37,True)
+				BottomHall=GPIO.input(31)
+				runTime=time.clock()-TimeStart
+		if 45==runTime:
+				down = '0'
+				print 'Something went wrong, go check the door!'
+				message = "Coop close FAILED!"
+				PushOver(message)
+		if BottomHall==0:
+				down = '0'
+				time.sleep(1)
+				print 'Door is locked!'
+				message = "Coop closed successfully!"
+				PushOver(message)
+
 #Web Server Config
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -74,81 +153,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-#Clean kill of script function (Stops Motor, cleans GPIO)
-def killSystem(): #Shutdown is queued
-        print 'Performing safe shutoff of Door & Server!'
-        GPIO.output(22,False)
-        GPIO.output(23,False)
-        GPIO.output(24,False)
-        GPIO.output(25,False)
-        GPIO.cleanup()
-        sys.exit('Motors shutdown, GPIO cleaned, server killed')
-
-#PushOver Config
-
-#config.txt included in .gitignore first line is the token, the second line is the key
-config = open('config.txt').readlines()
-pushover_token=config[0].rstrip()
-pushover_user=config[1]
-
-def PushOver(message):
-    conn = httplib.HTTPSConnection("api.pushover.net:443")
-    conn.request("POST", "/1/messages.json",
-      urllib.urlencode({
-        "token": pushover_token,
-        "user": pushover_user,
-        "message": message,
-      }), { "Content-type": "application/x-www-form-urlencoded" })
-    conn.getresponse()
-
-#Start door!
-
-TimeStart=time.clock()
-runTime=0
-
-BottomHall=GPIO.input(18)
-TopHall=GPIO.input(17)
-if BottomHall==0:print 'Door is locked'
-if TopHall==0:print 'Door is open'
-if BottomHall==1:print 'No magnet sensed on lock'
-if TopHall==1:print 'No magnet sensed top'
-def openDoor(): 
-	if BottomHall==0: #Door is locked
-		print 'The door is locked!'
-		print 'The door is going up!'
-		while TopHall==1 and runTime<Door_Time:
-				GPIO.output(35,True)
-				GPIO.output(37,False)
-				TopHall=GPIO.input(33)
-				runTime=time.clock()-TimeStart
-		if 45==runTime:
-				up = '0'
-				print 'Something went wrong, go check the door!'
-				message = 'Coop open FAILED!'
-				PushOver(message)
-		if TopHall==0:
-				up = '0'
-				print 'Door is open!'
-				message = 'Coop opened successfully!'
-				PushOver(message)
-def closeDoor():
-	if TopHall==0: #Door is open
-		print 'The door is open!'
-		print 'The door is going down!'
-		while BottomHall==1 and runTime<Door_Time:
-				GPIO.output(35,False)
-				GPIO.output(37,True)
-				BottomHall=GPIO.input(31)
-				runTime=time.clock()-TimeStart
-		if 45==runTime:
-				down = '0'
-				print 'Something went wrong, go check the door!'
-				message = "Coop close FAILED!"
-				PushOver(message)
-		if BottomHall==0:
-				down = '0'
-				time.sleep(1)
-				print 'Door is locked!'
-				message = "Coop closed successfully!"
-				PushOver(message)
